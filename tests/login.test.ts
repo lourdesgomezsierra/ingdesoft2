50d425cf2c308bd9c5d92f7873db7c45c1b55390
lourdesgomezsierra <lourdesmariagomezsierra@gmail.com>
2025-03-26 12:38:58 -0300

    z

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -3,22 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Carga variables del .env
 
-/* const BASE_URL = process.env.BASE_URL as string;
+const BASE_URL = process.env.BASE_URL as string;
 const DNI_VALIDO = process.env.DNI_VALIDO as string;
 const PASS_VALIDO = process.env.PASS_VALIDO as string;
 const DNI_INVALIDO = process.env.DNI_INVALIDO as string;
-const PASS_INVALIDO = process.env.PASS_INVALIDO as string; */
+const PASS_INVALIDO = process.env.PASS_INVALIDO as string;
 
 test.describe('Pruebas de Login', () => {
     
     test('Caso exitoso: ingreso con credenciales válidas', async ({ page }) => {
-        await page.goto(`https://sistemacuenca.ucp.edu.ar/alumnosnotas/`);
-        await page.getByRole('textbox', { name: 'Usuario' }).fill('123');
-        await page.getByRole('textbox', { name: 'clave inicial' }).fill('p123$');
+        await page.goto(`${BASE_URL}`);
+        await page.getByRole('textbox', { name: 'Usuario' }).fill(DNI_VALIDO);
+        await page.getByRole('textbox', { name: 'clave inicial' }).fill(PASS_VALIDO);
         await page.getByRole('button', { name: 'Submit' }).click();
-        await page.getByRole('textbox', { name: 'Usuario' }).fill('456');
+        /*await page.getByRole('textbox', { name: 'Usuario' }).fill('456');
         await page.getByRole('textbox', { name: 'clave inicial' }).fill('p456$');
-        await page.getByRole('button', { name: 'Submit' }).click();
+        await page.getByRole('button', { name: 'Submit' }).click();*/
 
     }); 
 
